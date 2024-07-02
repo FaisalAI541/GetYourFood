272e6f1b2a3d6017dcb926a712cb9006b6d6338b
FiasalAI541 <faisalahmad4179@gmail.com>
2024-07-02 20:13:12 +0530

    Menu API correction

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018";
 
-export const MENU_API = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.9447899&lng=77.7403283&restaurantId="
+export const MENU_API = "https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D20.9447899%26lng%3D77.7403283%26restaurantId%3D"
 
 export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.9447899&lng=77.7403283&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
